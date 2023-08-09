5b0acb6363d5ee4c63705bbc10a49f3ee9ddb3e0
Lunik <lunik@tiwabbit.fr>
2023-08-09 21:21:30 +0200

    allow to remove laflem python venvs
    Signed-off-by: Lunik <lunik@tiwabbit.fr>

--- lib/laflem/collections/python/virtualenv/create.py
+++ lib/laflem/collections/python/virtualenv/create.py
@@ -5,6 +5,7 @@ This module is used to create a new virtualenv.
 import os
 import sys
 import subprocess
+import shutil
 
 from laflem.log import console
 from laflem.collections.base.helloworld import HelloWorldModule
@@ -39,6 +40,13 @@ class VirtualenvCreateModule(HelloWorldModule):
       dest="python_virtualenv_create_force",
       help="Force the creation of the virtualenv."
     )
+    parser.add_argument(
+      "--remove",
+      "-r",
+      action="store_true",
+      dest="python_virtualenv_create_remove",
+      help="Remove the virtualenv."
+    )
 
   def _main(self, *_args, **kwargs):
     '''
@@ -46,6 +54,14 @@ class VirtualenvCreateModule(HelloWorldModule):
     '''
     # pylint: disable=line-too-long
     venv_path = f"{os.path.expanduser('~')}/.{PACKAGE_NAME}/python/{VIRTUALENV_DEFAULT_FOLDER_NAME}/{kwargs['python_virtualenv_create_name']}"
+
+    if (os.path.exists(venv_path) and kwargs['python_virtualenv_create_force']) or kwargs['python_virtualenv_create_remove']:
+      console.print(f"Removing [bold red]Virtualenv[/] at [bold green]{venv_path}[/]")
+      shutil.rmtree(venv_path)
+
+    if kwargs['python_virtualenv_create_remove']:
+      return
+
     if not os.path.exists(venv_path) or kwargs['python_virtualenv_create_force']:
       console.print(f"Creating [bold blue]Virtualenv[/] at [bold green]{venv_path}[/]")
       subprocess.check_call([sys.executable, "-m", "venv", venv_path])
